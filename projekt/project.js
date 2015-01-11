de37485d1babe3a0fd23d2c7b37e4a695e836584
Ben L. <e1025108@users.noreply.github.com>
2015-01-11 16:08:26 +0100

    Update project.js

--- projekt/project.js
+++ projekt/project.js
@@ -33,8 +33,7 @@ var CarSharing = new ol.layer.Vector({
     projection: 'EPSG:3857'
   }),
     style: new ol.style.Style({
-       image: new ol.style.Icon({src: 'http://data.wien.gv.at/katalog/images/carsharingogd.png',})
-       size: [16, 16],
+       image: new ol.style.Icon({src: 'http://data.wien.gv.at/katalog/images/carsharingogd.png', size: [16, 16],})
     })
 });
 
