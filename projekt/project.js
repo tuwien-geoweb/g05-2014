2d3b3827fa887e1d2ab854baf786b4b3444a1887
Ben L. <e1025108@users.noreply.github.com>
2015-01-11 16:30:07 +0100

    Update project.js

--- projekt/project.js
+++ projekt/project.js
@@ -33,7 +33,7 @@ var CarSharing = new ol.layer.Vector({
     projection: 'EPSG:3857'
   }),
     style: new ol.style.Style({
-       image: new ol.style.Icon({src: 'http://data.wien.gv.at/katalog/images/carsharingogd.png', })
+       image: new ol.style.Icon({src: 'http://student.ifip.tuwien.ac.at/geoweb/2014/g05/page/img/carsharingogd.png', })
     })
 });
 
@@ -43,7 +43,7 @@ var Haltestellen = new ol.layer.Vector({
     projection: 'EPSG:3857'
   }),
     style: new ol.style.Style({
-       image: new ol.style.Icon({src: 'http://data.wien.gv.at/katalog/images/haltestellewlogd.png',})
+       image: new ol.style.Icon({src: 'http://student.ifip.tuwien.ac.at/geoweb/2014/g05/page/img/haltestellewlogd.png',})
     })
 });
 
@@ -53,7 +53,7 @@ var Maerkte = new ol.layer.Vector({
     projection: 'EPSG:3857'
   }),
     style: new ol.style.Style({
-       image: new ol.style.Icon({src: 'http://data.wien.gv.at/katalog/images/marktogd.png',})
+       image: new ol.style.Icon({src: 'http://student.ifip.tuwien.ac.at/geoweb/2014/g05/page/img/marktogd.png',})
     })
 });
 
