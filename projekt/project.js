1388a718d36bd8a9906418ebb3a173d040d4556f
Ben L. <e1025108@users.noreply.github.com>
2015-01-13 15:57:20 +0100

    Update project.js

--- projekt/project.js
+++ projekt/project.js
@@ -33,7 +33,7 @@ var CarSharing = new ol.layer.Vector({
     projection: 'EPSG:3857'
   }),
     style: new ol.style.Style({
-       image: new ol.style.Icon({src: 'http://student.ifip.tuwien.ac.at/geoweb/2014/g05/page/img/carsharingogd.png', })
+       image: new ol.style.Icon({src: 'http://student.ifip.tuwien.ac.at/geoweb/2014/g05/page/img/carsharingogd.png', scale: 0.5 })
     })
 });
 
@@ -43,7 +43,7 @@ var Haltestellen = new ol.layer.Vector({
     projection: 'EPSG:3857'
   }),
     style: new ol.style.Style({
-       image: new ol.style.Icon({src: 'http://student.ifip.tuwien.ac.at/geoweb/2014/g05/page/img/haltestellewlogd.png',})
+       image: new ol.style.Icon({src: 'http://student.ifip.tuwien.ac.at/geoweb/2014/g05/page/img/haltestellewlogd.png', scale: 0.5})
     })
 });
 
@@ -53,7 +53,7 @@ var Maerkte = new ol.layer.Vector({
     projection: 'EPSG:3857'
   }),
     style: new ol.style.Style({
-       image: new ol.style.Icon({src: 'http://student.ifip.tuwien.ac.at/geoweb/2014/g05/page/img/marktogd.png',})
+       image: new ol.style.Icon({src: 'http://student.ifip.tuwien.ac.at/geoweb/2014/g05/page/img/marktogd.png', scale: 0.5})
     })
 });
 
